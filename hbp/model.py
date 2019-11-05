defc02ec2184e4eae241db9e989b5d19d2a7eda6
innabotchkina <innabotchkina@yahoo.com>
2019-11-05 21:05:36 +0000

    First commit model.py, login and register routes

---
+++ hbp/model.py
@@ -0,0 +1,121 @@
+"""Models and database functions for Ratings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting
+# this through the Flask-SQLAlchemy helper library. On this, we can
+# find the `session` object, where we do most of our interactions
+# (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+#####################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of activity log website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    username = db.Column(db.String(20), nullable=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+    
+    # XXXXXX ?what data type for google account? XXXXXX
+    # google_account = db.Column(db.Integer, nullable=True)
+    # google_account = db.Column(db.String(15), nullable=True)
+
+    def __repr__(self):
+        """Provide helpful representation of user when printed."""
+
+        return f"""<User       user_id={self.user_id} 
+            username={self.username}
+            email={self.email}>"""
+
+
+class Activity(db.Model):
+    """Movie on ratings website."""
+
+    __tablename__ = "activities"
+
+    act_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+    act_name = db.Column(db.String(30))
+    act_unit = db.Column(db.String(15))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    # ?? act_type = db.Column(db.Boolean)
+
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("activities",
+                                              order_by=act_id))
+    def __repr__(self):
+        """Provide helpful representation of an activity when printed."""
+
+        return f"""<Activity   act_id={self.act_id} 
+            act_name={self.act_name}
+            act_unit={self.act_unit}>"""
+
+
+class Event(db.Model):
+    """Instance (event) of activity by a user."""
+
+    __tablename__ = "events"
+
+    event_id = db.Column(db.Integer,
+                          autoincrement=True,
+                          primary_key=True)
+    act_id = db.Column(db.Integer,
+                         db.ForeignKey('activities.act_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    event_amt = db.Column(db.Float)
+    event_date = db.Column(db.DateTime) # ??? timestamp?
+    
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("events",
+                                              order_by=event_id))
+
+    # Define relationship to activity
+    activity = db.relationship("Activity",
+                            backref=db.backref("activities",
+                                               order_by=act_id))
+
+    def __repr__(self):
+        """Provide helpful representation of an event of activity when printed."""
+
+        return f"""<Event      event_id={self.event_id}
+            act_id={self.act_id}
+            user_id={self.user_id}
+            event_date={self.event_date}
+            event_amt={self.event_amt}>"""
+
+
+#####################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///actlog'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will
+    # leave you in a state of being able to work with the database
+    # directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
---
+++ hbp/server.py
@@ -0,0 +1,124 @@
+"""Activity Log"""
+
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db, User, Activity, Event
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+app.jinja_env.undefined = StrictUndefined
+
+
+#########################
+# http://0.0.0.0:5000/
+#########################
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+    # return "<html><body>Placeholder for the homepage.</body></html>"
+
+
+###############################
+# http://0.0.0.0:5000/register
+###############################
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Show form for user signup."""
+
+    return render_template("register.html")
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Process registration."""
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+    username = request.form["username"]
+
+    new_user = User(email=email, password=password, username=username)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    flash(f"User {email} added.")
+    return redirect("/")
+
+
+###############################
+# http://0.0.0.0:5000/login
+###############################
+@app.route('/login', methods=['GET'])
+def login_form():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process login."""
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("No such user")
+        return redirect("/login")
+
+    if user.password != password:
+        flash("Incorrect password")
+        return redirect("/login")
+
+    session["user_id"] = user.user_id
+
+    flash("Logged in")
+    return redirect("/")
+    # return redirect(f"/users/{user.user_id}")
+
+
+@app.route('/logout')
+def logout():
+    """Log out."""
+
+    del session["user_id"]
+    flash("Logged Out.")
+    return redirect("/")
+
+
+
+###############################
+# http://0.0.0.0:5000/activity
+###############################
+@app.route('/activity', methods=['GET'])
+def act():
+    """Form test page."""
+
+    return render_template("activity.html")
+
+
+
+
+
+if __name__ == "__main__":
+
+    app.debug = True
+
+    connect_to_db(app)
+
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
