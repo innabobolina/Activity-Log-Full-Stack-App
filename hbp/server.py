b3f27f536ab5eab295362927170c1c3c9a717a12
innabotchkina <innabotchkina@yahoo.com>
2019-11-07 23:36:44 +0000

    Activity wip, reg, login fixed

--- hbp/server.py
+++ hbp/server.py
@@ -28,29 +28,20 @@ def index():
 
 
 
-###############################
-# http://0.0.0.0:5000/login
-###############################
-# @app.route('/login', methods=['GET'])
-# def login_form():
-#     """Show login form."""
-
-#     return render_template("login.html")
-
 
 @app.route('/', methods=['POST'])
 def login_process():
     """Process login."""
 
     # Get form variables
-    username = username.form["username"]
+    username = request.form["username"]
     email = request.form["email"]
     password = request.form["password"]
 
     user = User.query.filter_by(email=email).first()
 
     if not user:
-        flash("No such user")
+        flash("No such user, please register")
         return redirect("/")
 
     if user.password != password:
@@ -60,7 +51,10 @@ def login_process():
     session["user_id"] = user.user_id
 
     flash("Logged in")
-    return redirect("/")
+    print("logged in", email)
+    return render_template("activity.html")
+
+    # return redirect("/")
     # return redirect(f"/users/{user.user_id}")
 
 
@@ -87,16 +81,23 @@ def register_process():
     """Process registration."""
 
     # Get form variables
-    email = request.form["email"]
+    email    = request.form["email"]
     password = request.form["password"]
     username = request.form["username"]
+    
+    user_exists = User.query.filter_by(email=email).first()
+
+    if user_exists:
+        flash(f"""User {email} already exists. 
+            Please log in, you don't need to register! :)""")
+        return redirect("/")
 
     new_user = User(email=email, password=password, username=username)
 
     db.session.add(new_user)
     db.session.commit()
 
-    flash(f"User {email} added.")
+    flash(f"User {email} added. Now please log in.")
     return redirect("/")
 
 
@@ -120,12 +121,20 @@ def get_activity():
     activity = request.form["activity"]
     unit = request.form["unit"]
 
-    new_act = Activity(act_name=activity, act_unit=unit)
+    act_exists = Activity.query.filter_by(act_name=activity, act_unit=unit).first()
 
-    db.session.add(new_act)
-    db.session.commit()
+    if act_exists:
+        flash("Activity already exists")
 
-    flash(f"Activity {activity} added.")
+    if not act_exists:
+
+        new_act = Activity(act_name=activity, act_unit=unit)
+
+        db.session.add(new_act)
+        db.session.commit()
+
+        flash(f"Activity {activity} added.")
+    print(act_exists)
     return redirect("/")
 
 # @app.route('/')
